b35e6685cfd5dbd59d79a66e33ad97b36689e77e
nlewis84 <github email address>
2021-06-04 13:51:29 -0500

    added options to drawer

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -2,12 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 
 import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
 import ActionsList from "../screens/ActionsList";
 import ActionDetails from "../screens/ActionDetails";
+import Settings from "../screens/Settings";
 
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
@@ -78,8 +80,20 @@ const AppTabsScreen = () => (
   </AppTabs.Navigator>
 );
 
+const AppDrawer = createDrawerNavigator();
+const AppDrawerScreen = () => (
+  <AppDrawer.Navigator drawerPosition="left">
+    <AppDrawer.Screen
+      name="Tabs"
+      component={AppTabsScreen}
+      options={{ drawerLabel: "Home" }}
+    />
+    <AppDrawer.Screen name="Settings" component={Settings} />
+  </AppDrawer.Navigator>
+);
+
 export default () => (
   <NavigationContainer>
-    <AppTabsScreen />
+    <AppDrawerScreen />
   </NavigationContainer>
 );
